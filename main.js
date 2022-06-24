1aec19ab511e17ad41b5c3b13966529cf4ddaf48
Kauê Cavalcante <102473287+kauecavalcante@users.noreply.github.com>
2022-06-24 16:37:44 -0300

    Update main.js
    corrigindo bugs e falhas (scroll and menu)
    
    Co-Authored-By: César Aranha <91692973+cesar-aranha@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
+const scrollHeader = () =>{
     const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the 
-    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -50,26 +51,25 @@ const toggleItem = (item) => {
     }
 }
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id')
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
-function scrollActive() {
-    const scrollY = window.pageYOffset
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop -58,
-        sectionId = current.getAttribute('id')
-
-        if(scollY > sectionTop && scrollY + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
 }
-
 window.addEventListener('scroll', scrollActive)
-
 /*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
